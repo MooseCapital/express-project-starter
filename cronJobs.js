8393342d411943f5385aff9529ab0484589e5874
Moose Capital <48196495+MooseCapital@users.noreply.github.com>
2023-11-26 06:35:19 -0600

    cron jobs setup

--- cronJobs.js
+++ cronJobs.js
@@ -1,7 +1,7 @@
 const cron = require('node-cron');
 
 const initScheduledJobs = () => {
-      const testJob = cron.schedule(" */1 * * * *", () => {
+      const testJob = cron.schedule(" */5 * * * *", () => {
         console.log("cron test");
         // runs every 1 minute, if we put ("1 * * * *") -> it would run every minute 1 of every hour
             //-> to run every second we would do "*/1 * * * * *"
