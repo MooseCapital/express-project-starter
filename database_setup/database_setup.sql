23a54facdf9491457fea476bbd017b4f232f9427
Moose Capital <48196495+MooseCapital@users.noreply.github.com>
2023-11-26 20:08:18 -0600

    postgres setup file

--- database_setup/database_setup.sql
+++ database_setup/database_setup.sql
@@ -2,41 +2,38 @@
 --connect to psql or run with db connected
 
 --in psql: *** must type in passwords below
-create role adminmooselocal login createdb;
-\password (password_here)
-create role safemoose login;
-\password (password_here)
-create role readonlymoose login;
-\password (password_here)
+create role adminmooselocal login createdb password 'i7Vp0H0bArQrwZvMbDfX';
+create role safemoose login password 'U26zHJ9B0Pl9IQVzDPag';
+create role readonlymoose login password 'p6pb0VxYCQ69TpC0Fs4S';
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
-
-create database mainDB owner adminmooselocal encoding "utf8";
-\connect mainDB adminmooselocal
+create database maindb owner adminmooselocal encoding "utf8";
+\connect maindb adminmooselocal
 
 -- Create all tables and objects, and after that:
 
-\connect mainDB postgres
+\connect maindb postgres
 
---revoke connect on database mainDB from public;
+--revoke connect on database maindb from public;
 revoke all on schema public from public;
 revoke all on all tables in schema public from public;
 
 --local only user with all privileges
-grant connect on database mainDB to adminmooselocal;
+grant connect on database maindb to adminmooselocal;
 grant all on schema public to adminmooselocal;
 grant all on all tables in schema public to adminmooselocal;
 grant all on all sequences in schema public to adminmooselocal;
 grant all on all functions in schema public to adminmooselocal;
 
 --normal external user with select, update, insert
-grant connect on database mainDB to safemoose;
+grant connect on database maindb to safemoose;
 grant usage on schema public to safemoose;
-grant select, insert, update on all tables in schema public to safemoose;
+grant select, insert, update, references, trigger on all tables in schema public to safemoose;
 grant usage, select on all sequences in schema public to safemoose;
 grant execute on all functions in schema public to safemoose;
 
 --read only user
-grant connect on database mainDB to readonlymoose;
+grant connect on database maindb to readonlymoose;
 grant usage on schema public to readonlymoose;
 grant select on all tables in schema public to readonlymoose;
 grant usage, select on all sequences in schema public to readonlymoose;
