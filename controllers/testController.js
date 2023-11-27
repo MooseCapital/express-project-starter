318f636309e2aab39febb5c03def6949f18267e6
Moose Capital <48196495+MooseCapital@users.noreply.github.com>
2023-11-27 16:36:57 -0600

    postgres setup file

--- controllers/testController.js
+++ controllers/testController.js
@@ -30,8 +30,30 @@ const test = (async (req, res) => {
     }
 })
 
+const ip = (async (req, res) => {
+        try {
+        let ip = req.ip; // trust proxy sets ip to the remote client (not to the ip of the last reverse proxy server)
+          if (ip.substr(0,7) === '::ffff:') { // fix for if you have both ipv4 and ipv6
+            ip = ip.substr(7);
+          }
+          // req.ip and req.protocol are now set to ip and protocol of the client, not the ip and protocol of the reverse proxy server
+          // req.headers['x-forwarded-for'] is not changed
+          // req.headers['x-forwarded-for'] contains more than 1 forwarder when
+          // there are more forwarders between the client and nodejs.
+          // Forwarders can also be spoofed by the client, but
+          // app.set('trust proxy') selects the correct client ip from the list
+          // if the nodejs server is called directly, bypassing the trusted proxies,
+          // then 'trust proxy' ignores x-forwarded-for headers and
+          // sets req.ip to the remote client ip address
 
-module.exports = {getPage, test}
+             res.json({"ip": ip, "protocol": req.protocol, "headers": req.headers['x-forwarded-for']});
+        } catch (e) {
+          console.log(e)
+          return res.status(500).json({error:'could not fetch'})
+        }
+})
+
+module.exports = {getPage, test, ip}
 
 
 
--- routes/testRouter.js
+++ routes/testRouter.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { test, getPage} = require("../controllers/testController");
+const { test, getPage, ip} = require("../controllers/testController");
 
 /* GET users listing. */
 router.get('/' , test)
 
 router.get('/page' , getPage)
-
+router.get('/ip',ip)
 
 module.exports = router;
